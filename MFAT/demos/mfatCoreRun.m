e72f4f02184f3cc601d83dce2a3d8145d65bfc93
dops0035 <dops0035@BIO-W-LT-02528>
2026-02-20 12:13:55 +0000

    removing direct calls to mfatfrac

--- MFAT/demos/mfatCoreRun.m
+++ MFAT/demos/mfatCoreRun.m
@@ -1,4 +1,4 @@
 outProb = mfatCore2D(I, sigmas, @mfatResponseProb);
 outLam  = mfatCore2D(I, sigmas, @mfatResponseLambda);
-outFrac = mfatCore2D(I, sigmas, @mfatResponseFrac);
-outEnt  = mfatCore2D(I, sigmas, @mfatResponseEntropy);
+% outFrac = mfatCore2D(I, sigmas, @mfatResponseFrac);
+% outEnt  = mfatCore2D(I, sigmas, @mfatResponseEntropy);
